7d9a07945cd0c8379ae8ffadc5a8e4e8be39f1c0
Loy <loys343@pop-os.localdomain>
2020-08-16 03:13:05 -0500

    imageDraw typo

--- hello_epaper.py
+++ hello_epaper.py
@@ -14,7 +14,7 @@ def printToDisplay(string):
     HBlackImage = Image.new('1', (epd2in9b_V2.EPD_HEIGHT, epd2in9b_V2.EPD_WIDTH), 225)
     HRedImage = Image.new('1', (epd2in9b_V2.EPD_HEIGHT, epd2in9b_V2.EPD_WIDTH,), 225)
 
-    draw = ImageDraw(HBlackImage) #Create draw object and pass in the image layer we want to work with HBlackImage
+    draw = ImageDraw.Draw(HBlackImage) #Create draw object and pass in the image layer we want to work with HBlackImage
     font = ImageFont.truetype('/usr/share/fonts/truetype/google/Bangers-Regular.ttf', 30) #Create our font, passing in the font file and font size
     draw.text((25, 65), string, font = font, fill = 0)
     epd.display(epd.getbuffer(HBlackImage), epd.getbuffer(HRedImage))
