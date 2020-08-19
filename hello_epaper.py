28e07eb63bf12c7b0ebb65baa557d5593c839cfa
Loy <loys343@pop-os.localdomain>
2020-08-19 15:23:26 -0500

    added get request

--- hello_epaper.py
+++ hello_epaper.py
@@ -1,9 +1,13 @@
 import sys
 # sys.path.insert(1, "./lib") # Adds lib folder in this directory to sys
+import requests
+import json
+
 
 from lib import epd2in9b_V2
 
 from PIL import Image, ImageDraw, ImageFont
+
 
 epd = epd2in9b_V2.EPD() #get the display
 epd.init()              #initialize the display
@@ -18,9 +22,16 @@ def printToDisplay(string):
     font = ImageFont.truetype('/usr/share/fonts/Hack-Regular.ttf', 30) #Create our font, passing in the font file and font size
     draw.text((25, 65), string, font = font, fill = 0)
     epd.display(epd.getbuffer(HBlackImage), epd.getbuffer(HRedImage))
+def getCryptoData():
+    response = requests.get('http://api.coincap.io/v2/assets?ids=bitcoin')
+    resData = json.loads(response.text)
+    data = resData['data'][0]
+    name = data['name']
+    return name
 
 def handleBtnPress():
-    message = input()
-    printToDisplay(message)
+    # message = input()
+    # printToDisplay(message)
+    printToDisplay(getCryptoData())
 
 handleBtnPress()
