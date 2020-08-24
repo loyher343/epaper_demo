bca869b294f402ac21845b1f6583a64a3fdff0b2
loyher343 <loyhernandez59@gmail.com>
2020-08-24 23:31:38 +0100

    changed font size and position

--- hello_epaper.py
+++ hello_epaper.py
@@ -19,9 +19,9 @@ def printToDisplay(string):
     HRedImage = Image.new('1', (epd2in9b_V2.EPD_HEIGHT, epd2in9b_V2.EPD_WIDTH,), 225)
 
     draw = ImageDraw.Draw(HBlackImage) #Create draw object and pass in the image layer we want to work with HBlackImage
-    font = ImageFont.truetype('/usr/share/fonts/Hack-Regular.ttf', 30) #Create our font, passing in the font file and font size
-    #draw.text((25, 5), string, font = font, fill = 0)
-    draw.multiline_text((5, 45), string, font = font, fill = 0, spacing= 4, align="center")
+    font = ImageFont.truetype('/usr/share/fonts/Hack-Regular.ttf', 15) #Create our font, passing in the font file and font size
+    #draw.text((0, 0), string, font = font, fill = 0)
+    draw.multiline_text((0, 0), string, font = font, fill = 0, spacing= 4, align="center")
     epd.display(epd.getbuffer(HBlackImage), epd.getbuffer(HRedImage))
 def getCryptoData():
     response = requests.get('http://api.coincap.io/v2/assets?ids=ethereum')
@@ -35,7 +35,7 @@ def getCryptoData():
 
 def handleBtnPress():
     # message = input()
-    #printToDisplay("LOY HERNANSDEZ")
-    printToDisplay(getCryptoData())
+    printToDisplay("Tis better to Vile \n than Vile esteem")
+    #printToDisplay(getCryptoData())
 
 handleBtnPress()
